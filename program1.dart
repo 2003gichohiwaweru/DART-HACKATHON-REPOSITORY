a7c4ad4aef3c385d7583835ea3f1e6a735d85266
Joseph Gichohi <josephgichohi322@gmail.com>
2024-03-17 01:40:57 +0300

    My Updates built, and executed programs

---
+++ program1.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Gichohi Joseph";
+  int age = 24;
+  String school = "The Technical University of Kenya";
+  String hobby = "listening to music";
+
+  print(
+      "My name is $name. I am $age years old, and I attend $school. My hobby is $hobby.");
+}
