67387226786619f03acfe43357f31f6e07958287
Joseph Gichohi <josephgichohi322@gmail.com>
2024-03-17 01:30:08 +0300

    Updates

--- functions.dart
+++ functions.dart
@@ -1,24 +1,52 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Enter numbers separated by space: ");
-  String wekwa = stdin.readLineSync()!;
+  while (true) {
+    print("\nMenu:");
+    print("1. Addition");
+    print("2. Multiplication");
+    print("3. Quit");
 
-  List<double> numbers = wekwa.split(' ').map((e) => double.parse(e)).toList();
+    stdout.write("Enter your choice: ");
+    String chaguo = stdin.readLineSync()!;
 
-  double sum = addition(numbers);
-  print("Sum: $sum");
-
-  double product = multiply(numbers);
-  print("Product: $product");
+    switch (chaguo) {
+      case '1':
+        toPerfomOperatin("addition");
+        break;
+      case '2':
+        toPerfomOperatin("multiplication");
+        break;
+      case '3':
+        print("Goodbye!");
+        return;
+      default:
+        print("Invalid choice. Please enter a number from 1 to 3.");
+        break;
+    }
+  }
 }
 
-// Function to perform addition
-double addition(List<double> numbers) {
+void toPerfomOperatin(String operation) {
+  stdout.write("Enter numbers separated by space: ");
+  String input = stdin.readLineSync()!;
+
+  List<double> numbers = input.split(' ').map((e) => double.parse(e)).toList();
+
+  double result;
+  if (operation == "addition") {
+    result = add(numbers);
+    print("Sum: $result");
+  } else if (operation == "multiplication") {
+    result = multiply(numbers);
+    print("Product: $result");
+  }
+}
+
+double add(List<double> numbers) {
   return numbers.reduce((value, element) => value + element);
 }
 
-// Function to perform multiplication
 double multiply(List<double> numbers) {
   return numbers.reduce((value, element) => value * element);
 }
